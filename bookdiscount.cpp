2efcebc51ad824ba842bec9a0cb81b46c15842ef
Muhammad Fairuz Bin Herman <mfairuz06@gmail.com>
2025-10-16 23:22:32 +0800

    Add files via upload

---
+++ bookdiscount.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	double oriPrice;
+	cout<< "Price of the book : "<< endl;
+	cin >> oriPrice;
+	
+	double pricediscount;
+	pricediscount = oriPrice - (oriPrice * 0.35);
+	cout << "Price after discount : "<<pricediscount;
+	
+	
+	
+	
+	
+}
